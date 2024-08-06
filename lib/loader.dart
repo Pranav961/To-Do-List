9c7d56dcfa328755a808a8f4e4b3ce301b3b9e1e
Pranav0333 <pranavpatel033.frontbitstd@gmail.com>
2024-08-06 11:40:03 +0530

    init

--- lib/loader.dart
+++ lib/loader.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 void showLoadingDialog(context) {
   showDialog(
     context: context,
-    builder: (context) => const Center(
-      child: CircularProgressIndicator(backgroundColor: Colors.white,color: Color(0xFF0192ED)),
+    builder: (context) =>  Center(
+      // child: CircularProgressIndicator(backgroundColor: Colors.white,color: Color(0xFF0192ED)),
+      child: LoadingAnimationWidget.inkDrop(
+        color: Colors.white,
+        size: 40,
+      ),
     ),
   );
 }
